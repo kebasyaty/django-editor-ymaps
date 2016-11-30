770a93ae80ca3599a4c8efa779187fbc21d57bb5
genkosta <genkosta43@gmail.com>
2016-11-30 18:17:10 +0200

    Update __init__

--- yandex_maps/__init__.py
+++ yandex_maps/__init__.py
@@ -1,3 +1,8 @@
-__author__ = 'genkosta'
+# coding: utf-8
+"""
+    Creating and editing Yandex maps.
+"""
 
+__author__ = 'genkosta'
+__version__ = '0.1'
 default_app_config = 'yandex_maps.apps.YandexMapsConfig'

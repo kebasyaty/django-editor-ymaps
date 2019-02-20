ea73907f8c040b853bf22979b377a9b983142043
genkosta <genkosta43@gmail.com>
2019-02-20 15:19:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,44 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  Copyright (c) 2018 genkosta
+#  Copyright (c) 2014 genkosta
 #
 #  django-editor-ymaps is free software under terms of the MIT License.
 #
 
-import os
-import re
-from setuptools import setup, find_packages
-
-
-def read(*parts):
-    file_path = os.path.join(os.path.dirname(__file__), *parts)
-    with open(file_path, encoding='utf-8') as target_file:
-        result = target_file.read()
-    return result
-
-
-def find_version(*parts):
-    version_file = read(*parts)
-    version_match = re.search(
-        r'''^__version__ = ['"]([^'"]*)['"]''', version_file, re.M)
-    if version_match:
-        return str(version_match.group(1))
-    raise RuntimeError('Unable to find version string.')
+from setuptools import find_packages, setup
 
 
 def get_readme(file_path):
-    with open(file_path, encoding='utf-8') as readme_file:
+    with open(file_path) as readme_file:
         result = readme_file.read()
     return result
 
 
 setup(
     name='django-editor-ymaps',
-    version=find_version('yandex_maps', '__init__.py'),
+    version='1.0 beta',
     packages=find_packages(),
     include_package_data=True,
-    requires=['python (>= 3.6)', 'django (>= 2.1)'],
+    requires=['python (>= 3.6)', 'django (>= 2.0)'],
     description='Creating and editing Yandex maps.',
     long_description=get_readme('README.rst'),
     author='genkosta',
@@ -46,7 +28,7 @@ setup(
     url='https://github.com/genkosta/django-editor-ymaps',
     download_url='https://github.com/genkosta/django-editor-ymaps/tarball/master',
     license='MIT License',
-    keywords='django editor ymaps yandex maps',
+    keywords='django yandex map maps djeym',
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
@@ -54,12 +36,16 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     install_requires=[
-        'Pillow==5.2.0',
-        'django-imagekit==4.0.2',
-        'python-slugify==1.2.6',
-        'django-smart-selects==1.5.4',
-        'django-ckeditor==5.6.1',
+        'Django',
+        'Pillow',
+        'django-imagekit',
+        'python-slugify',
+        'django-ckeditor',
+        'lxml',
+        'django-smart-selects==1.5.3',
+        'django-ipware'
     ],
 )

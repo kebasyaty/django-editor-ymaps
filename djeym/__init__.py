ac645d8fde3de1e16f5b9d3f1858f69505a69312
genkosta <genkosta43@gmail.com>
2019-04-12 09:07:24 +0300

    Update __init__.py

--- djeym/__init__.py
+++ djeym/__init__.py
@@ -7,7 +7,7 @@ $$__$$__$$__$$__$$_______$$____$$___$$
 $$$$$____$$$$___$$$$$____$$____$$___$$
 """
 
-VERSION = (1, 2, 15)
+VERSION = (1, 2, 16)
 
 __title__ = 'DjEYM (django-editor-ymaps)'
 __version__ = '.'.join(map(str, VERSION))

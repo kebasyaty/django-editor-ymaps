f5d666747015e9a9764b4fec097fcce893ea0c1a
kebasyaty <kebasyaty@gmail.com>
2023-09-21 15:52:43 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ from os import path
 from setuptools import find_packages, setup
 
 
-VERSION = (2, 3, 1)
+VERSION = (2, 3, 2)
 __version__ = '.'.join(map(str, VERSION))
 
 
@@ -69,7 +69,7 @@ setup(
     ],
     install_requires=[
         'Django>=3.2.4',
-        'Pillow==9.5.0',
+        'Pillow==9.*',
         'django-imagekit',
         'python-slugify',
         'django-ckeditor',

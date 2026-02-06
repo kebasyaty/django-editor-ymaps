40c0d8034c4dcdb0d2546e0e85e7932fc4e618e8
kebasyaty <kebasyaty@gmail.com>
2026-02-06 21:17:10 +0200

    update comment to a django_editor_ymaps module

--- src/django_editor_ymaps/__init__.py
+++ src/django_editor_ymaps/__init__.py
@@ -1,9 +1,9 @@
-# :::::::-.  .,::::::.-:.     ::-..        :
-#  ;;,   `';,;;;;'''' ';;.   ;;;;';;,.    ;;;
-#  `[[     [[ [[cccc    '[[,[[['  [[[[, ,[[[[,
-#   $$,    $$ $$""""      c$$"    $$$$$$$$"$$$
-#   888_,o8P' 888oo,__  ,8P"`     888 Y88" 888o
-#   MMMMP"`   """"YUMMMmM"        MMM  M'  "MMM
+# :::::::-.      ....::::::.,::::::.-:.     ::-..        :
+#  ;;,   `';, ;;;;;;;;;````;;;;'''' ';;.   ;;;;';;,.    ;;;
+#  `[[     [[ ''`  `[[.     [[cccc    '[[,[[['  [[[[, ,[[[[,
+#   $$,    $$,,,    `$$     $$""""      c$$"    $$$$$$$$"$$$
+#   888_,o8P'888boood88     888oo,__  ,8P"`     888 Y88" 888o
+#   MMMMP"`  "MMMMMMMM"     """"YUMMMmM"        MMM  M'  "MMM
 #
 # Django-Editor-YMaps - Plugin for creating and editing Yandex maps with Django framework.
 # Copyright (c) 2014 Gennady Kostyunin

cc2809dca9118be0aaf8a3b368f1081c06b860d7
kebasyaty <kebasyaty@gmail.com>
2026-02-07 19:01:06 +0200

    update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -13,6 +13,10 @@ from __future__ import annotations
 
 from pathlib import Path
 
+from django.utils.translation import gettext_lazy as _
+
+PROJECT_NAME: str = "app"
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +30,9 @@ SECRET_KEY = "django-insecure-w#t@o6v2cugc24z@d-n(_&+vf8_#04%^y^%pcbs2ta1b9)xi31
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+
+ADMINS = [("admin", "kebasyaty@gmail.com")]
 
 
 # Application definition
@@ -38,16 +44,28 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # add-ons
+    "imagekit",
+    "ckeditor",
+    "ckeditor_uploader",
+    "colorful",
+    "adminsortable",
+    # apps
+    "app",
+    "djeym",
 ]
 
 MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
+    # "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django.middleware.locale.LocaleMiddleware",
+    # custom middlewares
+    "djeym.middleware.AjaxMiddleware",
 ]
 
 ROOT_URLCONF = "app.urls"
@@ -55,19 +73,27 @@ ROOT_URLCONF = "app.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR.joinpath("templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
+                "django.template.context_processors.debug",
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "django.template.context_processors.i18n",
+                "django.template.context_processors.media",
+                "django.template.context_processors.static",
+                "django.template.context_processors.tz",
             ],
+            "debug": DEBUG,
         },
     },
 ]
 
 WSGI_APPLICATION = "app.wsgi.application"
+
+DEFAULT_AUTO_FIELD = "django.db.models.AutoField"
 
 
 # Database
@@ -103,7 +129,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/6.0/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "ru"
+
+LANGUAGES = [("en", _("English")), ("ru", _("Russian")), ("uk", _("Ukrainian")), ("tr", _("Turkish"))]
+
+LOCALE_PATHS = (BASE_DIR.joinpath("locale"),)
+
+SITE_ID = 1
 
 TIME_ZONE = "UTC"
 
@@ -116,3 +148,84 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATIC_ROOT = str(BASE_DIR / "djeym/static")
+
+# Additional locations of static files
+STATICFILES_DIRS = (BASE_DIR.joinpath("static"),)
+
+# List of finder classes that know how to find static files in
+# various locations.
+STATICFILES_FINDERS = (
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder",
+    # 'django.contrib.staticfiles.finders.DefaultStorageFinder',
+)
+
+
+# Media files
+MEDIA_URL = "media/"
+MEDIA_ROOT = str(BASE_DIR.joinpath("media"))
+
+# Email
+EMAIL_HOST = "localhost"
+EMAIL_PORT = 1025
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
+EMAIL_USE_TLS = False
+DEFAULT_FROM_EMAIL = "noreply@site.net"
+
+TEST_RUNNER = "django.test.runner.DiscoverRunner"
+
+DATA_UPLOAD_MAX_MEMORY_SIZE = 4194304
+
+# django-ckeditor
+# https://github.com/django-ckeditor/django-ckeditor
+CKEDITOR_BASEPATH = "/static/ckeditor/ckeditor/"
+CKEDITOR_UPLOAD_PATH = "uploads/"
+CKEDITOR_FILENAME_GENERATOR = "djeym.utils.get_filename"
+CKEDITOR_THUMBNAIL_SIZE = (300, 300)
+CKEDITOR_FORCE_JPEG_COMPRESSION = True
+CKEDITOR_IMAGE_QUALITY = 40
+CKEDITOR_IMAGE_BACKEND = "pillow"
+# False - Only image files. (At your discretion)
+CKEDITOR_ALLOW_NONIMAGE_FILES = False
+CKEDITOR_CONFIGS = {
+    "default": {
+        "toolbar": "full",
+        "height": 400,
+        "width": "100%",
+    },
+    "djeym": {
+        "toolbar": "full",
+        "height": 400,
+        "width": 362,
+        "colorButton_colors": "F44336,C62828,E91E63,AD1457,9C27B0,6A1B9A,"
+        "673AB7,4527A0,3F51B5,283593,2196F3,1565C0,"
+        "03A9F4,0277BD,00BCD4,00838F,009688,00695C,"
+        "4CAF50,2E7D32,8BC34A,558B2F,CDDC39,9E9D24,"
+        "FFEB3B,F9A825,FFC107,FF8F00,FF9800,EF6C00,"
+        "FF5722,D84315,795548,4E342E,607D8B,37474F,"
+        "9E9E9E,424242,000000,FFFFFF",
+        "colorButton_enableAutomatic": False,
+        "colorButton_enableMore": True,
+    },
+}
+
+# If a non-authenticated user requests an editor page.
+LOGIN_URL = "admin/"  # or change to your URL
+
+# Required for django-admin-sortable
+# https://github.com/alsoicode/django-admin-sortable#configuration
+CSRF_COOKIE_HTTPONLY = False
+
+# The API key is used in the free and paid versions.
+# You can get the key in the developer’s office - https://passport.yandex.com/
+DJEYM_YMAPS_API_KEY = "aadb75d5-cd13-4d2d-8c0b-79e8d1d0f19a"
+
+# For paid use API --> True
+DJEYM_YMAPS_API_KEY_FOR_ENTERPRISE = False
+
+# Map download mode.
+# Default = "release".
+DJEYM_YMAPS_DOWNLOAD_MODE = "debug" if DEBUG else "release"

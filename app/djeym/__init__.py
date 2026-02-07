72b5ea8a2ea33600d9d04062f1a8e8bcfcae4336
kebasyaty <kebasyaty@gmail.com>
2026-02-07 19:53:26 +0200

    update app/djeym/__init__.py

--- app/djeym/__init__.py
+++ app/djeym/__init__.py
@@ -11,3 +11,21 @@
 """Convenient use of the Yandex map service for web development on the popular and free Django framework."""
 
 from __future__ import annotations
+
+DjEYM_VERSION = (3, 0, 0)  # noqa: RUF067
+PYTHON_VERSION = (3, 12)  # noqa: RUF067
+DJANGO_VERSION = (5, 0, 0)  # noqa: RUF067
+VUE_VERSION = (2, 7, 14)  # noqa: RUF067
+VUETIFY_VERSION = (2, 2, 1)  # noqa: RUF067
+
+__title__ = "DjEYM (django-editor-ymaps)"  # noqa: RUF067
+__djeym_version__ = ".".join(map(str, DjEYM_VERSION))  # noqa: RUF067
+__python_version__ = ".".join(map(str, PYTHON_VERSION))  # noqa: RUF067
+__django_version__ = ".".join(map(str, DJANGO_VERSION))  # noqa: RUF067
+__vue_version__ = ".".join(map(str, VUE_VERSION))  # noqa: RUF067
+__vuetify_version__ = ".".join(map(str, VUETIFY_VERSION))  # noqa: RUF067
+__author__ = "kebasyaty"
+__license__ = "MIT"  # noqa: RUF067
+__copyright__ = "Copyright (c) 2014 kebasyaty - Gennady Kostyunin"  # noqa: RUF067
+
+default_app_config = "djeym.apps.DjeymConfig"  # noqa: RUF067

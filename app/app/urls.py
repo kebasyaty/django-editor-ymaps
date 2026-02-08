04d3648e33dfe052a56a89ec935ed82d693209ea
kebasyaty <kebasyaty@gmail.com>
2026-02-08 06:23:22 +0200

    fix app/app/urls.py

--- app/app/urls.py
+++ app/app/urls.py
@@ -26,7 +26,11 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("ckeditor/", include("ckeditor_uploader.urls")),
     path("djeym/", include("djeym.urls", namespace="djeym")),
-    #
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]
+
+if settings.DEBUG:
+    (
+        urlpatterns
+        + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+        + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    )

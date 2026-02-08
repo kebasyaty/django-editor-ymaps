bf2ade20bc7e6da3fed0b0e9ab92f8770f965f40
kebasyaty <kebasyaty@gmail.com>
2026-02-08 06:15:21 +0200

    fix app/djeym/apps.py

--- app/djeym/apps.py
+++ app/djeym/apps.py
@@ -1,9 +1,11 @@
+"""Apps."""
+
 from __future__ import annotations
 
 from django.apps import AppConfig
 from django.utils.translation import gettext_lazy as _
 
 
-class DjeymConfig(AppConfig):
+class DjeymConfig(AppConfig):  # noqa: D101
     name = "djeym"
     verbose_name = _("Yandex maps")

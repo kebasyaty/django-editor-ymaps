6d9a645bf461e700dc7209564be49bdedd929cd9
kebasyaty <kebasyaty@gmail.com>
2026-02-08 07:17:29 +0200

    fix app/djeym/middleware.py

--- app/djeym/middleware.py
+++ app/djeym/middleware.py
@@ -1,14 +1,15 @@
+"""Middleware."""
+
 from __future__ import annotations
 
 
-class AjaxMiddleware:
-    def __init__(self, get_response):
+class AjaxMiddleware:  # noqa: D101
+    def __init__(self, get_response):  # noqa: D107
         self.get_response = get_response
 
-    def __call__(self, request):
-        def is_ajax(self):
+    def __call__(self, request):  # noqa: D102
+        def is_ajax(self):  # noqa: ARG001
             return request.META.get("HTTP_X_REQUESTED_WITH") == "XMLHttpRequest"
 
         request.is_ajax = is_ajax.__get__(request)
-        response = self.get_response(request)
-        return response
+        return self.get_response(request)

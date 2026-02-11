fbf227685e845c6ca8381ffe530c8967672211ab
kebasyaty <kebasyaty@gmail.com>
2026-02-11 17:41:12 +0200

    update app/app/urls.py

--- app/app/urls.py
+++ app/app/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("ckeditor/", include("ckeditor_uploader.urls")),
     path("djeym/", include("djeym.urls", namespace="djeym")),
+    #
     *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
 ]

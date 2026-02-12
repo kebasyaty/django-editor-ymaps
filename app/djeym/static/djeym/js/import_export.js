d9435ce3404d109005adbee6e196c81f64f3c4c5
kebasyaty <kebasyaty@gmail.com>
2026-02-13 01:03:33 +0200

    fix djeym/js/import_export.js

--- app/djeym/static/djeym/js/import_export.js
+++ app/djeym/static/djeym/js/import_export.js
@@ -1,111 +1,99 @@
 /*
-* Import/Export - Icon Collection and Source tile layer.
-* Copyright (c) 2014 kebasyaty
-* License MIT
-*/
+ * Import/Export - Icon Collection and Source tile layer.
+ * Copyright (c) 2014 kebasyaty
+ * License MIT
+ */
 
-$( document ).ready( function() {
-  'use strict';
+$(document).ready(function () {
+  "use strict";
 
-  $( '#changelist .actions' ).css( 'margin-top', '20px' );
+  $("#changelist .actions").css("margin-top", "20px");
 
   // Start load indication
-  let TIMEOUT_SPIN_INDICATION;
   function startLoadIndication() {
-    $( '#djeymModalLock' ).show().find( '#djeymLoadIndicator' ).addClass( 'djeym-load-indicator' );
-    TIMEOUT_SPIN_INDICATION = setTimeout( function() {
-      if ( $( '#djeymModalLock' ).css( 'display' ) !== 'none' ) {
-        stopLoadIndication();
-      }
-    }, 30000 );
+    $("#djeymModalLock")
+      .show()
+      .find("#djeymLoadIndicator")
+      .addClass("djeym-load-indicator");
   }
 
-  // Stop load indication
-  function stopLoadIndication() {
-    const $modalLock = $( '#djeymModalLock' );
-    $modalLock.css( 'opacity', '0' );
-    setTimeout( function() {
-      $modalLock.find( '#djeymLoadIndicator' ).removeClass( 'djeym-load-indicator' );
-      $modalLock.hide().css( 'opacity', '.85' );
-      clearTimeout( TIMEOUT_SPIN_INDICATION );
-    }, 1000 );
-  }
-
-  function sandFormAjax( url, formData ) {
-    $.ajax( {
-      type: 'POST',
+  function sandFormAjax(url, formData) {
+    startLoadIndication();
+    $.ajax({
+      type: "POST",
       url: url,
       data: formData,
       processData: false,
-      contentType: false
-    } )
-      .done( function() {
-        stopLoadIndication();
-        document.location.reload( true );
-      } )
-      .fail( function( jqxhr, textStatus, error ) {
-        const err = textStatus + ', ' + error;
-        let errDetail = '';
+      contentType: false,
+    })
+      .done(function () {
+        document.location.reload(true);
+      })
+      .fail(function (jqxhr, textStatus, error) {
+        const err = textStatus + ", " + error;
+        let errDetail = "";
 
-        stopLoadIndication();
-
-        if ( jqxhr.responseJSON !== undefined &&
-          jqxhr.responseJSON.detail !== undefined ) {
+        if (
+          jqxhr.responseJSON !== undefined &&
+          jqxhr.responseJSON.detail !== undefined
+        ) {
           errDetail = jqxhr.responseJSON.detail;
         }
 
-        if ( errDetail.length !== 0 ) {
-          console.log( 'Request Failed: ' + err + ' - ' + errDetail );
-          alert( ( 'Request Failed: ' + err + ' - ' + errDetail ) );
+        if (errDetail.length !== 0) {
+          console.log("Request Failed: " + err + " - " + errDetail);
+          alert("Request Failed: " + err + " - " + errDetail);
         } else {
-          console.log( 'Request Failed: ' + err );
-          alert( 'Request Failed: ' + err );
+          console.log("Request Failed: " + err);
+          alert("Request Failed: " + err);
         }
-      } );
+      });
   }
 
-  if ( $( 'a' ).is( '#id_import_icon_collection' ) ) { //
+  if ($("a").is("#id_import_icon_collection")) {
+    //
     // Import file Icon Collection.
-    $( '#id_import_file_icon_collection' ).on( 'change', function() {
-      const url = $( '#id_import_icon_collection' ).data( 'import-url' );
-      const collectionFile = $( this )[ 0 ].files[ 0 ];
-      const csrftoken = $( 'input[name="csrfmiddlewaretoken"' ).val();
+    $("#id_import_file_icon_collection").on("change", function () {
+      const url = $("#id_import_icon_collection").data("import-url");
+      const collectionFile = $(this)[0].files[0];
+      const csrftoken = $('input[name="csrfmiddlewaretoken"').val();
       const formData = new FormData();
 
-      formData.append( 'csrfmiddlewaretoken', csrftoken );
-      formData.append( 'collection', collectionFile );
+      formData.append("csrfmiddlewaretoken", csrftoken);
+      formData.append("collection", collectionFile);
 
-      startLoadIndication();
-      sandFormAjax( url, formData );
-    } );
-  } else if ( $( 'a' ).is( '#id_import_tile_source' ) ) { //
-    if ( ( $( 'table#result_list tr' ).length - 1 ) > 0 ) {
-      $( '#id_export_tile_source' ).show();
+      sandFormAjax(url, formData);
+    });
+  } else if ($("a").is("#id_import_tile_source")) {
+    //
+    if ($("table#result_list tr").length - 1 > 0) {
+      $("#id_export_tile_source").show();
     }
 
     // Import file with sources of tile layers.
-    $( '#id_import_file_tile_source' ).on( 'change', function() {
-      const url = $( '#id_import_tile_source' ).data( 'import-url' );
-      const sourcesFile = $( this )[ 0 ].files[ 0 ];
-      const csrftoken = $( 'input[name="csrfmiddlewaretoken"' ).val();
+    $("#id_import_file_tile_source").on("change", function () {
+      const url = $("#id_import_tile_source").data("import-url");
+      const sourcesFile = $(this)[0].files[0];
+      const csrftoken = $('input[name="csrfmiddlewaretoken"').val();
       const formData = new FormData();
 
-      formData.append( 'csrfmiddlewaretoken', csrftoken );
-      formData.append( 'sources', sourcesFile );
+      formData.append("csrfmiddlewaretoken", csrftoken);
+      formData.append("sources", sourcesFile);
 
-      startLoadIndication();
-      sandFormAjax( url, formData );
-    } );
+      sandFormAjax(url, formData);
+    });
   }
 
-  if ( $( 'input' ).is( '#id_site' ) ) {
-    const $site = $( '#id_site' );
-    if ( $site.val().length > 0 ) {
+  if ($("input").is("#id_site")) {
+    const $site = $("#id_site");
+    if ($site.val().length > 0) {
       const url = $site.val();
-      const btn = '<div class="tile_go_to_website_btn"><a href="' + url +
+      const btn =
+        '<div class="tile_go_to_website_btn"><a href="' +
+        url +
         '" target="_blank" rel="nofollow noreferrer noopener">' +
         '<img src="/static/djeym/img/arrow_right.svg" alt="Button"></a></div>';
-      $site.after( btn );
+      $site.after(btn);
     }
   }
-} );
+});
